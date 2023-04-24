1cc8332eab3ac2bbbfcb2ddce18fd5ec778f1b1c
HiepPD <hieppdutck59@gmail.com>
2023-04-24 16:21:27 +0700

    Use android:windowBackground in Theme set background SplashScreen in Android

--- app/src/main/java/com/example/masterchef/SplashAct.kt
+++ app/src/main/java/com/example/masterchef/SplashAct.kt
@@ -10,7 +10,7 @@ class SplashAct: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.act_splash)
+//        setContentView(R.layout.act_splash)
 
         estimateTimeAnimation()
 
